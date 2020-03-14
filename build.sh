fa45a75c0e90ced412fbe09d19b3a97f1196e17e
Jensen David <feudavid92@gmail.com>
2020-03-13 18:04:13 -0700

    updated other files with appropriate content

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 cat templates/top.html content/contact.html templates/bottom.html > fullcontactpage.html
+
+cat templates/top.html content/index.html templates/bottom.html > fullindexpage.html
+
+cat templates/top.html content/projects.html templates/bottom.html > fullprojectspage.html
