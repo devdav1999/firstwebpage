fc720e4ce2548e9e980f3f516a4d022308478013
Jensen David <feudavid92@gmail.com>
2020-03-13 18:10:09 -0700

    updated build.sh to place new documents to docs folder

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cat templates/top.html content/contact.html templates/bottom.html > fullcontactpage.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/fullcontactpage.html
 
-cat templates/top.html content/index.html templates/bottom.html > fullindexpage.html
+cat templates/top.html content/index.html templates/bottom.html > docs/fullindexpage.html
 
-cat templates/top.html content/projects.html templates/bottom.html > fullprojectspage.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/fullprojectspage.html
