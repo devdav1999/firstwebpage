57e5688ed3739905b0f05cb1e56bebbad95b8149
Jensen David <feudavid92@gmail.com>
2020-03-13 18:30:52 -0700

    updated every file for docs folder

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cat templates/top.html content/contact.html templates/bottom.html > docs/fullcontactpage.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/index.html
 
-cat templates/top.html content/index.html templates/bottom.html > docs/fullindexpage.html
+cat templates/top.html content/index.html templates/bottom.html > docs/contact.html
 
-cat templates/top.html content/projects.html templates/bottom.html > docs/fullprojectspage.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
