f1b1796aa3d5c19435262258459bf528b8f8db0e
Jensen David <feudavid92@gmail.com>
2020-03-14 21:57:11 -0700

    updated main directory with updated files

--- build.py
+++ build.py
@@ -3,18 +3,18 @@ top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 middle_html = open('content/index.html').read()
 combined_html = top_html + middle_html + bottom_html
-open('docs/index.html', 'w+').write(combined html)
+open('docs/index.html', 'w+').write(combined_html)
 
 print('building static site')
 top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 middle_html = open('content/projects.html').read()
 combined_html = top_html + middle_html + bottom_html
-open('docs/projects.html', 'w+').write(combined html)
+open('docs/projects.html', 'w+').write(combined_html)
 
 print('building static site')
 top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 middle_html = open('content/contact.html').read()
 combined_html = top_html + middle_html + bottom_html
-open('docs/projects.html', 'w+').write(combined html)
+open('docs/contact.html', 'w+').write(combined_html)
